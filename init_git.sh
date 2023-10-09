b98b167fff25afa4cc7a47a222effe14ad16cb6c
tbxy09 <test@test.com>
2023-10-09 14:53:25 +0800

    Initial commit

--- init_git.sh
+++ init_git.sh
@@ -12,8 +12,8 @@ git commit -m "Initial commit"
 # Set the remote to http://150.109.60.238:3000/tbxy09/MetaAgent.git with name and password
 git config --local user.name "$USER_NAME"
 git config --local user.email "test@test.com"
-git config --local user.password $PASSWORD
+# git config --local user.password $PASSWORD
 git remote add origin $GOGS/$USER_NAME/MetaAgent.git
 
 # Push the changes to the remote repository
-git push -u origin master
+git push -u origin 
