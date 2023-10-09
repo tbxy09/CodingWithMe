80f3bec78dcc5b67899c0fb958a7e3cc5a6564e9
tbxy09 <tbxy09@gmail.com>
2023-10-09 17:46:17 +0800

    finish the clone_function test code

--- src/main.py
+++ src/main.py
@@ -1,14 +1,45 @@
-from metagpt import MetaGPT
+import asyncio
+from importlib.abc import InspectLoader
+from metagpt.actions.clone_function import CloneFunction
+from importlib.abc import InspectLoader
 
-def main():
-    # Initialize MetaGPT
-    metagpt = MetaGPT()
+class MyLoader(InspectLoader):
+    def get_source(self, fullname):
+        # provide an implementation for this method
+        pass
 
-    # Perform actions using MetaGPT
-    result = metagpt.do_action()
+inspect_loader = MyLoader()
 
-    # Print result
-    print(result)
+# Original function 
+source_code = """
+def original_function(x, y):
+    return x + y
+"""
 
-if __name__ == '__main__':
-    main()
+# Function template
+template_func = "def new_function(a, b):\\n    # New implementation here\\n"
+
+# Clone into new function 
+async def clone_function():
+    cf = CloneFunction()
+    # print(inspect_loader.get_code(original_function.__module__))
+    new_function_code = await cf.run(template_func=template_func, source_code=source_code)
+    print(inspect_loader.get_code(CloneFunction.__module__))
+    print(new_function_code)
+
+# Run the asynchronous function
+# asyncio.run(clone_function())
+import inspect
+
+# Get the module object from the module name
+module = inspect.importlib.import_module(CloneFunction.__module__)
+
+# Get the source file path of the module
+source_path = inspect.getsourcefile(module)
+
+# Read the source code from the file
+with open(source_path, "r") as f:
+    source_code = f.read()
+
+# Print the source code
+print(source_code)
